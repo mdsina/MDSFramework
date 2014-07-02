95876bac047d551f3134a55acac113ae5d8100d3
mdsina <info@mmmcreative.ru>
2014-07-03 00:16:40 +0700

    Добавил одиночку для соединения
    + вынес интерфейс соединения для фабрики

---
+++ Framework/DataSource/QueryInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Base interface for working with query
+ * @author Daniil Mikhailov <info@mdsina.ru>
+ * @copyright Copyright (c) 2014, Daniil Mikhailov
+ */
+
+interface Framework_DataSource_QueryInterface
+{
+    public function where($field, $value);
+    public function groupBy($groups);
+    public function select($fields);
+    public function limit($limit);
+    public function raw($query);
+}
